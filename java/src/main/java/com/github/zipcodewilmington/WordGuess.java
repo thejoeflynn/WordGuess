23367e07e3ea560af8c6f816b6efbfd18a21b523
thejoeflynn <thejoeflynn@gmail.com>
2026-04-13 22:43:38 -0400

    Phase One (kinda) complete. Java won't run the program.

--- java/src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -1,8 +1,29 @@
 package com.github.zipcodewilmington;
 
+import java.util.Scanner;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
-public class WordGuess {}
+public class WordGuess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Word Guess!");
+        System.out.println("Go ahead and guess a letter. Or type 'Quit' to quit the game.");
+
+        String input = "";
+
+    while (!input.equalsIgnoreCase("Quit")) {
+        input = scanner.nextLine();
+        if (input.equalsIgnoreCase("Quit")) {
+            System.out.println("Thanks for playing!");
+            break;
+        } else {
+            System.out.println(input);
+        }
+    } 
+    }
+}
