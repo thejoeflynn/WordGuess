654675145713773d1e13c66ca223d9e091a30cf9
thejoeflynn <thejoeflynn@gmail.com>
2026-04-14 23:01:24 -0400

    updated Person

--- java/src/main/java/com/github/zipcodewilmington/sample/Person.java
+++ java/src/main/java/com/github/zipcodewilmington/sample/Person.java
@@ -1,4 +1,44 @@
 package com.github.zipcodewilmington.sample;
 
 public class Person {
+    private String name;
+    private int score;
+    private int wins;
+    private int losses;
+
+    public Person(String name) {
+        this.name = name;
+        this.score = 0;
+        this.wins = 0;
+        this.losses = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLosses () {
+        return losses;
+    }
+
+    public void addWin () {
+        wins++;
+        score++;
+    }
+
+    public void addLoss () {
+        losses++;
+        score--;
+    }
+
+
+
 }
